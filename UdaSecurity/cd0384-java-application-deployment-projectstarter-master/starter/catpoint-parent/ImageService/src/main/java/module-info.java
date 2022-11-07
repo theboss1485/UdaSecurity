96a2ce8f9fc6b0d88c6a1b54d95046dfe5be6738
theboss1485 <86418893+theboss1485@users.noreply.github.com>
2022-11-06 20:27:27 -0500

    Delete UdaSecurity/cd0384-java-application-deployment-projectstarter-master directory

--- UdaSecurity/cd0384-java-application-deployment-projectstarter-master/starter/catpoint-parent/ImageService/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module ImageService {
-    requires java.desktop;
-    requires org.slf4j;
-    requires software.amazon.awssdk.services.rekognition;
-    requires software.amazon.awssdk.auth;
-    requires software.amazon.awssdk.regions;
-    requires software.amazon.awssdk.core;
-    exports serviceimage;
-}
