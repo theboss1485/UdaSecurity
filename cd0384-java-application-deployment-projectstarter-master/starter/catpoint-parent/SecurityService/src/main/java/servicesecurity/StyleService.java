ce27d0b767df3e8156cbeb0be4cbcf72e9a53171
theboss1485 <86418893+theboss1485@users.noreply.github.com>
2022-11-13 18:56:43 -0500

    Add files via upload

---
+++ cd0384-java-application-deployment-projectstarter-master/starter/catpoint-parent/SecurityService/src/main/java/servicesecurity/StyleService.java
@@ -0,0 +1,12 @@
+package servicesecurity;
+
+import java.awt.*;
+
+/**
+ * Simple "servicesecurity" for providing style information.
+ */
+public class StyleService {
+
+    public static final Font HEADING_FONT = new Font("Sans Serif", Font.BOLD, 24);
+
+}
